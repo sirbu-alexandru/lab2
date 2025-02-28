3b058508d366cb48f1923428bca01137c3b90d08
MoisaGabriel <gabriel.moisa@student.tuiasi.ro>
2025-02-28 09:32:49 +0200

    hazul romaniei

--- src/bref/haisalut.java
+++ src/bref/haisalut.java
@@ -5,7 +5,7 @@ public class haisalut {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hhazai");
-		System.out.println("Andrei e prost");
+		System.out.println("Andrei e prost1pahah");
 	}
 
 }
